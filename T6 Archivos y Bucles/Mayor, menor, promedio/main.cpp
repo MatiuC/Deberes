9a3a4940059c44ceec7add8b04ceb4305a7a8593
MatiuC <eduardo.castro01@epn.edu.ec>
2024-06-08 18:32:44 -0500

    Primer ejercicio v1

---
+++ T6 Archivos y Bucles/Mayor, menor, promedio/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int num, contador=0, num1, acum,mayor,menor;
+    ifstream file; //Def del .txt
+    file.open("numeros.txt");
+    if (!file.is_open()){
+        cout << "El archivo no existe";
+        return 1;
+    }
+        //contar cuantos numeros hay
+        file >> num;  //primer numero (necesario, si no el while suma 1 más)
+        while (num !=0){
+            file >> num1;
+            if (num1>num){
+                mayor = num1;
+            }
+            if (num1<num && num1!=0){
+                menor = num1;
+            }
+            num = num1;
+            contador++;
+            acum = acum + num;
+        }
+    
+
+        cout << "El mayor de los numeros es: " << mayor <<endl;
+        cout << "El menor de los numeros es: " << menor <<endl;
+        cout << "El promedio de los numeros es: " << acum/contador << endl;
+    return 0;
+
+}
