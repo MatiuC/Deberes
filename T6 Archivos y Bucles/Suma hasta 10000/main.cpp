c4be1ef39f381da570d3d05ef3da24b4e1a3e7d4
MatiuC <eduardo.castro01@epn.edu.ec>
2024-06-08 19:24:48 -0500

    ejercicios 2 v1

--- T6 Archivos y Bucles/Suma hasta 10000/main.cpp
+++ T6 Archivos y Bucles/Suma hasta 10000/main.cpp
@@ -3,8 +3,24 @@
 using namespace std;
 
 int main(){
-    fstream file;
-
+    int a=1,b=2, suma=a+b, ultimonum=b;
+    char aux;
+    ofstream file; //Def del .txt
+    file.open("sumas.txt"); //Crea el archivo
+    if (!file.is_open()){
+        cout << "El archivo no se pudo crear";
+        return 1;
+    }
     
+     while (suma < 10000) {
+       int temp = a + b;
+       a=b;
+       b=temp;
+       suma=a+b;
+       ultimonum=b;
+       file << a << '+' << b << "= " << suma << endl;
+    }
+    cout << "El ultimo numero sumado fue: " << ultimonum;
+    cout << "\n Puedes revisar las sumas en el archivo sumas.txt";
     return 0;
 }
